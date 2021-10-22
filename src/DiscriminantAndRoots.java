e86bfc49b0431bf368a446541b8ce8dc46f0569c
AveryCohen <AveryCohen04@gmail.com>
2021-10-22 11:55:59 -0400

    worked on else if and finished exersize 1

--- src/DiscriminantAndRoots.java
+++ src/DiscriminantAndRoots.java
@@ -1,9 +1,32 @@
+import java.util.Scanner;
+
 public class DiscriminantAndRoots {
 
     // Create the method roots in the space below.
     public static String roots(int a, int b, int c) {
+        double discriminant = Math.pow((double)(b), 2) -4 * (a * c);
+        if(discriminant > 0) {
+            String tworeal = a + "x^2 + " + b + "x + " + c + " has 2 real roots";
+            return tworeal;
+        }
+        else if(discriminant == 0) {
+            String onereal = a + "x^2 + " + b + "x + " + c + " has 1 real root";
+            return onereal;
+        }
+        else {
+            String twoimagine = a + "x^2 + " + b + "x + " + c + " has 2 imaginary roots";
+            return twoimagine;
+        }
 
-        return ""; // update this line so that it returns properly formatted output
+
     }
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the coefficients for a quadratic equation: ");
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        System.out.println(roots(a, b, c));
 
+    }
 }
--- src/Palindrome.java
+++ src/Palindrome.java
@@ -1,4 +1,39 @@
+import java.util.Scanner;
+
 public class Palindrome {
+    public static boolean isPalindrome(int a) {
+        boolean palinyes = true;
+        boolean palinno = false;
+        int b = a % 10;
+        int c = (a % 100) - b;
+        int d = a / 10000;
+        int e = a / 1000;
+        if (a <= 99999) {
+            if (a >9999) {
+                if (b == d) {
+                    if (c == e) {
+                        return palinyes;
+                    }
+                    else {
+                        return palinno;
+                    }
+                }
+                else {
+                    return palinno;
+                }
+            }
+            else {
+                return palinno;
+            }
+        }
+        else {
+            return palinno;
+        }
+
+
+
+
+    }
 
     // Write your isPalindrome method in the space below. You do not need to write a main method for this program
     // Your method should be static.
