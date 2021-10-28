7b5026bc5e3539b342076bf8c26ce1552e853fdd
AveryCohen <AveryCohen04@gmail.com>
2021-10-28 08:59:12 -0400

    Finished LoneTeen and started Season

--- src/LoneTeen.java
+++ src/LoneTeen.java
@@ -1,4 +1,29 @@
+import java.util.Scanner;
+
 public class LoneTeen {
+    public static Boolean loneTeen(int a, int b) {
+        if (a>=13 && a<= 19) {
+            if (b >=13 && b<= 19) {
+                return false;
+            }
+            else {
+                return true;
+            }
+        }
+        else if (b >=13 && b<= 19) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the coefficients for a quadratic equation: ");
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        System.out.println(loneTeen(a, b));
+    }
 
     // In the space below, write the loneTeen method. Make sure the signature matches
     // the one given in the instructions.
--- src/Season.java
+++ src/Season.java
@@ -1,4 +1,25 @@
 public class Season {
 
+    public static String season(int a, int b) {
+        if (a >=1 && a<=3 || a==12) {
+            if (a==12 && b >=21) {
+                return "winter";
+            }
+            else if (a==3 && b <=19) {
+                return "winter";
+            }
+            else if (a==3 && b>19) {
+                return "spring";
+            }
+            else if (a ==12 && b <21) {
+                return "fall";
+            }
+            else {
+                return "winter";
+            }
+        }
+    }
+
+
 
 }
