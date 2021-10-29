b9ea35aa3999713fbdfaf8c68666f36c2c67bb23
AveryCohen <AveryCohen04@gmail.com>
2021-10-29 09:48:07 -0400

    Finished EasterDate

---
+++ src/EasterDate.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class EasterDate {
+    public static Boolean isValidYear(int year) {
+        if (year < 1900 || year == 1954 || year == 1981 || year == 2049 || year == 2076 || year > 2099) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+    public static String dateOfEaster(int year) {
+        if (isValidYear(year)) {
+            int a = year % 19;
+            int b = year % 4;
+            int c = year % 7;
+            int d = (19 * a + 24) % 30;
+            int e = (2*b + 4*c + 6*d + 5) % 7;
+            int date = 22 + d + e;
+            if (date > 31) {
+                String aprilDate = "In " + year + " Easter is on April " + (date - 31);
+                return  aprilDate;
+            }
+            else {
+                String marchDate = "In " + year + " Easter is on March " + date;
+                return  marchDate;
+            }
+        }
+        else {
+            return "You have entered an invalid year";
+        }
+    }
+
+
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter a year between 1900 and 2099");
+        int year = scan.nextInt();
+        System.out.println(dateOfEaster(year));
+    }
+
+}
